4cfaae8df2bd7cbcdbf7f5925241570872248ac8
Javante Nelson <javantenelson@gmail.com>
2020-04-27 06:32:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,2 +1,13 @@
 # Create your costume_stores migration here
 
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :costumes do |t|
+      t.string :name 
+      t.float :price  
+      t.string :size 
+      t.string :image_url 
+      t.timestamps 
+    end 
+  end 
+end 
